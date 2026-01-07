102aa0af186a29a89a4a97563e7853017c8ad21e
Ivanilson Félix <nil_si@yahoo.com.br>
2026-01-07 12:50:59 -0300

    Adicionando recursividade e desenvolvendo jogo da velha

---
+++ praticasProgramacao/js/jogoDaVelha.js
@@ -0,0 +1,112 @@
+let tabuleiro 
+let board
+let aviso
+let jogador
+let linha
+let coluna
+
+
+function iniciar(){
+    tabuleiro = []
+    board = document.getElementById("board")
+    aviso = document.getElementById("aviso")
+    jogador = 1
+
+    for (let i = 0; i < 3; i++){
+        tabuleiro [i] = []
+        for (let j = 0; j < 3; j++){
+            tabuleiro [i][j] = 0
+        }
+    }
+    console.table (tabuleiro)
+    exibir()
+
+}
+
+function exibir(){
+    let tabela = "<table cellpadding='10' border='1'>"
+
+    for (let i = 0; i < 3; i++){
+        tabela += "<tr>"
+
+        for (let j = 0; j < 3; j++){
+
+            switch (tabuleiro [i][j]){
+                case -1: marcador = "X"; break;
+                case 1: marcador = "O"; break;
+                default: marcador = "_"
+            }
+
+
+
+        tabela += "<td>" + marcador + "</td>"
+        }
+        
+        tabela += "</tr>"
+    }
+    tabela += "</table>"
+
+    board.innerHTML = tabela
+
+}
+        
+
+function jogar(){
+
+    aviso.innerHTML = "Vez do jogador: " + numeroJogador()
+
+    linha = document.getElementById("linha").value - 1
+    coluna = document.getElementById("coluna").value - 1
+
+    if (tabuleiro [linha][coluna] == 0){
+        tabuleiro [linha][coluna] = numeroJogador() == 1 ? 1 : -1
+    }else{
+        aviso.innerHTML = "Posição ocupada! Tente outra."
+    }
+    console.table(tabuleiro)
+    jogador++
+    exibir()
+}
+
+function checar(){
+
+    //Linhas
+
+    for (let i = 0; i < 3; i++){
+        let soma = 0
+        soma = tabuleiro [i][0] + tabuleiro [i][1] + tabuleiro [i][2]
+
+        if (soma == 3 || soma == -3){
+            aviso.innerHTML = "Jogador" + numeroJogador() + " venceu!"
+            
+        }
+    }
+
+    //Colunas
+
+    for (let j = 0; j < 3; j++){
+        let soma = 0
+        soma = tabuleiro [0][j] + tabuleiro [1][j] + tabuleiro [2][j]
+
+        if (soma == 3 || soma == -3){
+            aviso.innerHTML = "Jogador" + numeroJogador() + " venceu!"
+            
+        }
+    }
+
+    //Diagonais
+
+    let soma = 0
+    soma = tabuleiro [0][0] + tabuleiro [1][1] + tabuleiro [2][2]   
+
+    if (soma == 3 || soma == -3){
+        aviso.innerHTML = "Jogador" + numeroJogador() + " venceu!"
+    }                                                                                                                                                                                                           
+}
+
+function numeroJogador(){
+    return jogador%2 + 1
+}
+
+
+
